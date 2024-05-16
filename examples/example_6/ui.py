f1a8dc804f9eab04d0db844149923c679f35a481
Dene33 <den33.info@gmail.com>
2024-05-16 19:08:35 +0200

    Initial retargeting

--- examples/example_6/ui.py
+++ examples/example_6/ui.py
@@ -1,5 +1,51 @@
 import bpy
-from .operators import MOVE_SDK_OT_run
+from .operators import (
+    MOVE_SDK_OT_run,
+    MOVE_SDK_OT_retarget,
+    MOVE_SDK_OT_retargeting_clear,
+)
+
+
+def populate_panel_with_bones(
+    layout: bpy.types.UILayout,
+    scene: bpy.types.Scene,
+    rig_type: str,
+    ids: list = [],
+    add_label: bool = True,
+):
+    rig = getattr(scene.move_sdk.retargeting, rig_type).rig
+
+    if rig:
+        annotations_keys = list(
+            getattr(scene.move_sdk.retargeting, rig_type).__annotations__.keys()
+        )
+
+        for index, bone_prop_name in enumerate(annotations_keys):
+            if bone_prop_name in ["rig", "hips_original_transforms"]:
+                continue
+            if ids and index not in ids:
+                continue
+            layout.prop_search(
+                getattr(scene.move_sdk.retargeting, rig_type),
+                bone_prop_name,
+                rig,
+                "bones",
+                text=[bone_prop_name if add_label else ""][0],
+            )
+
+            # Check if the selected bone is valid
+            selected_bone = getattr(
+                getattr(scene.move_sdk.retargeting, rig_type), bone_prop_name
+            )
+            if selected_bone and selected_bone not in rig.bones:
+                row = layout.row()
+                row.alert = True
+                row.label(
+                    text=f"Bone {bone_prop_name} '{selected_bone}' not found!",
+                    icon="ERROR",
+                )
+    # else:
+    #     layout.label(text="No armature selected or invalid selection")
 
 
 class MOVE_SDK_PT_main_panel(bpy.types.Panel):
@@ -10,6 +56,17 @@ class MOVE_SDK_PT_main_panel(bpy.types.Panel):
     bl_category = "Move.ai SDK"
 
     def draw(self, context):
+        pass
+
+class MOVE_SDK_PT_general_panel(bpy.types.Panel):
+    bl_label = "API"
+    bl_idname = "MOVE_SDK_PT_general_panel"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Move.ai SDK"
+    bl_parent_id = "MOVE_SDK_PT_main_panel"
+
+    def draw(self, context):
         layout = self.layout
 
         layout.prop(context.scene.move_sdk.general, "move_api_key")
@@ -17,3 +74,228 @@ class MOVE_SDK_PT_main_panel(bpy.types.Panel):
         layout.prop(context.scene.move_sdk.general, "output_dir")
         layout.prop(context.scene.move_sdk.general, "import_fbx")
         layout.operator(MOVE_SDK_OT_run.bl_idname)
+
+
+class MOVE_SDK_PT_retargeting_panel(bpy.types.Panel):
+    bl_label = "Retargeting"
+    bl_idname = "MOVE_SDK_PT_retargeting_panel"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Move.ai SDK"
+    bl_parent_id = "MOVE_SDK_PT_main_panel"
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.operator(MOVE_SDK_OT_retarget.bl_idname)
+        layout.separator()
+
+        layout.operator(MOVE_SDK_OT_retargeting_clear.bl_idname)
+
+
+class MOVE_SDK_PT_retargeting_source_panel(bpy.types.Panel):
+    bl_label = "Source rig"
+    bl_idname = "MOVE_SDK_PT_retargeting_source_panel"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Move.ai SDK"
+    bl_parent_id = "MOVE_SDK_PT_retargeting_panel"
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+
+        layout.prop(scene.move_sdk.retargeting.source, "rig")
+
+        layout.separator()
+
+        populate_panel_with_bones(layout, scene, "source", ids=[1, 2, 3, 4])
+
+        layout.separator()
+
+        split = layout.split(factor=0.5)
+        column_left = split.column()
+        column_right = split.column()
+
+        populate_panel_with_bones(column_left, scene, "source", ids=[5, 6, 7, 8])
+        populate_panel_with_bones(column_right, scene, "source", ids=[9, 10, 11, 12])
+
+        layout.separator()
+
+        split = layout.split(factor=0.5)
+        column_left = split.column()
+        column_right = split.column()
+
+        populate_panel_with_bones(column_left, scene, "source", ids=[13, 14, 15, 16])
+        populate_panel_with_bones(column_right, scene, "source", ids=[17, 18, 19, 20])
+
+        if getattr(scene.move_sdk.retargeting, "source").rig:
+            layout.separator()
+
+            split = layout.split(factor=0.5)
+            column_left = split.column()
+            column_right = split.column()
+
+            row = column_left.row()
+            row.label(text="Right Thumb")
+            populate_panel_with_bones(
+                row, scene, "source", ids=[21, 22, 23], add_label=False
+            )
+
+            row = column_left.row()
+            row.label(text="Right Index")
+            populate_panel_with_bones(
+                row, scene, "source", ids=[24, 25, 26], add_label=False
+            )
+
+            row = column_left.row()
+            row.label(text="Right Middle")
+            populate_panel_with_bones(
+                row, scene, "source", ids=[27, 28, 29], add_label=False
+            )
+
+            row = column_left.row()
+            row.label(text="Right Ring")
+            populate_panel_with_bones(
+                row, scene, "source", ids=[30, 31, 32], add_label=False
+            )
+
+            row = column_left.row()
+            row.label(text="Right Pinky")
+            populate_panel_with_bones(
+                row, scene, "source", ids=[33, 34, 35], add_label=False
+            )
+
+            row = column_right.row()
+            row.label(text="Left Thumb")
+            populate_panel_with_bones(
+                row, scene, "source", ids=[36, 37, 38], add_label=False
+            )
+
+            row = column_right.row()
+            row.label(text="Left Index")
+            populate_panel_with_bones(
+                row, scene, "source", ids=[39, 40, 41], add_label=False
+            )
+
+            row = column_right.row()
+            row.label(text="Left Middle")
+            populate_panel_with_bones(
+                row, scene, "source", ids=[42, 43, 44], add_label=False
+            )
+
+            row = column_right.row()
+            row.label(text="Left Ring")
+            populate_panel_with_bones(
+                row, scene, "source", ids=[45, 46, 47], add_label=False
+            )
+
+            row = column_right.row()
+            row.label(text="Left Pinky")
+            populate_panel_with_bones(
+                row, scene, "source", ids=[48, 49, 50], add_label=False
+            )
+
+
+class MOVE_SDK_PT_retargeting_target_panel(bpy.types.Panel):
+    bl_label = "Target rig"
+    bl_idname = "MOVE_SDK_PT_retargeting_target_panel"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Move.ai SDK"
+    bl_parent_id = "MOVE_SDK_PT_retargeting_panel"
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+
+        layout.prop(scene.move_sdk.retargeting.target, "rig")
+
+        layout.separator()
+
+        populate_panel_with_bones(layout, scene, "target", ids=[1, 2, 3, 4])
+
+        layout.separator()
+
+        split = layout.split(factor=0.5)
+        column_left = split.column()
+        column_right = split.column()
+
+        populate_panel_with_bones(column_left, scene, "target", ids=[5, 6, 7, 8])
+        populate_panel_with_bones(column_right, scene, "target", ids=[9, 10, 11, 12])
+
+        layout.separator()
+
+        split = layout.split(factor=0.5)
+        column_left = split.column()
+        column_right = split.column()
+
+        populate_panel_with_bones(column_left, scene, "target", ids=[13, 14, 15, 16])
+        populate_panel_with_bones(column_right, scene, "target", ids=[17, 18, 19, 20])
+
+        if getattr(scene.move_sdk.retargeting, "target").rig:
+            layout.separator()
+
+            split = layout.split(factor=0.5)
+            column_left = split.column()
+            column_right = split.column()
+
+            row = column_left.row()
+            row.label(text="Right Thumb")
+            populate_panel_with_bones(
+                row, scene, "target", ids=[21, 22, 23], add_label=False
+            )
+
+            row = column_left.row()
+            row.label(text="Right Index")
+            populate_panel_with_bones(
+                row, scene, "target", ids=[24, 25, 26], add_label=False
+            )
+
+            row = column_left.row()
+            row.label(text="Right Middle")
+            populate_panel_with_bones(
+                row, scene, "target", ids=[27, 28, 29], add_label=False
+            )
+
+            row = column_left.row()
+            row.label(text="Right Ring")
+            populate_panel_with_bones(
+                row, scene, "target", ids=[30, 31, 32], add_label=False
+            )
+
+            row = column_left.row()
+            row.label(text="Right Pinky")
+            populate_panel_with_bones(
+                row, scene, "target", ids=[33, 34, 35], add_label=False
+            )
+
+            row = column_right.row()
+            row.label(text="Left Thumb")
+            populate_panel_with_bones(
+                row, scene, "target", ids=[36, 37, 38], add_label=False
+            )
+
+            row = column_right.row()
+            row.label(text="Left Index")
+            populate_panel_with_bones(
+                row, scene, "target", ids=[39, 40, 41], add_label=False
+            )
+
+            row = column_right.row()
+            row.label(text="Left Middle")
+            populate_panel_with_bones(
+                row, scene, "target", ids=[42, 43, 44], add_label=False
+            )
+
+            row = column_right.row()
+            row.label(text="Left Ring")
+            populate_panel_with_bones(
+                row, scene, "target", ids=[45, 46, 47], add_label=False
+            )
+
+            row = column_right.row()
+            row.label(text="Left Pinky")
+            populate_panel_with_bones(
+                row, scene, "target", ids=[48, 49, 50], add_label=False
+            )
--- examples/example_6/utils.py
+++ examples/example_6/utils.py
@@ -4,6 +4,9 @@ import requests
 from move_ugc import MoveUgc
 import subprocess
 import sys
+import bpy_types
+from mathutils import Matrix
+import numpy as np
 
 
 class MoveAPI:
@@ -66,3 +69,82 @@ class MoveAPI:
                 f.write(response.content)
             output_paths.append(output_path)
         return output_paths
+
+
+def add_constraint(
+    bone, name, constraint_type, target=None, subtarget=None, influence=1
+):
+    constraint = bone.constraints.new(constraint_type)
+    constraint.name = name
+    if target is not None:
+        constraint.target = target
+    if subtarget is not None:
+        constraint.subtarget = subtarget
+    constraint.influence = influence
+    return constraint
+
+
+def get_object_of_armature(armature_name):
+    armature = bpy.data.armatures.get(armature_name)
+    for obj in bpy.data.objects:
+        if obj.type == "ARMATURE":
+            if obj.data == armature:
+                return obj
+
+
+def get_matrix(bone_or_obj):
+    if isinstance(bone_or_obj, bpy_types.PoseBone):
+        # Convert matrix to world space
+        arm = bone_or_obj.id_data
+        mat = arm.matrix_world @ bone_or_obj.matrix
+    elif isinstance(bone_or_obj, Matrix):
+        mat = bone_or_obj
+    else:
+        mat = bone_or_obj.matrix_world
+
+    return mat
+
+
+def set_matrix(context: bpy_types.Context, mat: Matrix, bone_or_obj) -> None:
+    if isinstance(bone_or_obj, bpy_types.PoseBone):
+        # Convert matrix to local space
+        arm_eval = context.active_object.evaluated_get(context.view_layer.depsgraph)
+        bone_or_obj.matrix = arm_eval.matrix_world.inverted() @ mat
+    else:
+        context.active_object.matrix_world = mat
+
+
+def get_offsets_rot(source, target, rot_mode):
+    source_world = get_matrix(source)
+    target_world = get_matrix(target)
+
+    source_quat = source_world.to_quaternion()
+    target_quat = target_world.to_quaternion()
+    dif_quat = target_quat.rotation_difference(source_quat)
+    dif_euler = dif_quat.to_euler()
+
+    if rot_mode == "quaternion":
+        return dif_quat
+    if rot_mode == "euler":
+        return dif_euler
+
+
+def get_distance(start, end):
+    start_world = get_matrix(start)
+    end_world = get_matrix(end)
+    distance = np.linalg.norm(start_world.to_translation() - end_world.to_translation())
+    return distance
+
+
+def get_matrix_height(matrix_world: Matrix):
+    start_world = Matrix().to_translation()
+    translation = matrix_world.to_translation()
+    translation.x = 0
+    translation.y = 0
+    distance = np.linalg.norm(start_world - translation)
+    return distance
+
+
+def flatten_matrix(mat):
+    dim = len(mat)
+    return [mat[j][i] for i in range(dim) for j in range(dim)]
